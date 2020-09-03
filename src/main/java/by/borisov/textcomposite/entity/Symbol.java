98ec01b60bbcb3d5fe74cd1e2fa1e60e0a528717
MirniyPirojok <mirniypirojok@gmail.com>
2020-09-03 18:54:33 +0300

    Composite text. Add some methods to entity.

--- src/main/java/by/borisov/textcomposite/entity/Symbol.java
+++ src/main/java/by/borisov/textcomposite/entity/Symbol.java
@@ -2,10 +2,20 @@ package by.borisov.textcomposite.entity;
 
 public class Symbol implements TextComponent {
     private char symbol;
-    private ComponentType type;
+    private ComponentType componentType;
+
+    public Symbol(char symbol) {
+        this.symbol = symbol;
+        this.componentType = ComponentType.SYMBOL;
+    }
 
     @Override
     public void add(TextComponent component) {
-
     }
+
+    @Override
+    public ComponentType getComponentType() {
+        return componentType;
+    }
+
 }
--- src/main/java/by/borisov/textcomposite/entity/TextComponent.java
+++ src/main/java/by/borisov/textcomposite/entity/TextComponent.java
@@ -1,9 +1,8 @@
 package by.borisov.textcomposite.entity;
 
 public interface TextComponent {
+
     void add(TextComponent component);
 
-//    ComponentType getComponentType();
-
-
+    ComponentType getComponentType();
 }
